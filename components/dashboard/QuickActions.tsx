b487ab7501486f9f14b46e67a718dc001a2fffde
timiajeojo <timiajeojo@gmail.com>
2026-05-22 21:49:48 +0100

    create quick actions page

---
+++ components/dashboard/QuickActions.tsx
@@ -0,0 +1,18 @@
+
+import { quickActions } from "@/components/dashboard/dashboardData"
+
+export default function QuickActions() {
+  return (
+    <div className="grid grid-cols-4 gap-2 mb-5">
+      {quickActions.map((action) => (
+        <button 
+        type="buttom"
+        className="flex flex-col items-center gap-2 bg-white rounded-2xl py-4 border-none cursor-pointer hover:bg-gray-50 transition-colors shadow-sm"
+        >
+        <span className="text-gray-800">{action.icon}</span>
+          <span className="text-[12px] font-medium text-gray-700">{action.label}</span>
+        </button>
+        ))}
+    </div>
+    )
+}
